c9f44596859d686fa6882029c6449ca1401586e7
Amurov Anton <amurov2@gmail.com>
2021-06-23 09:17:18 +0300

    create task4

---
+++ task4.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+import argparse
+import sys
+import logging
+
+logging.basicConfig(filename='task4.log', filemode='a', level=logging.INFO)
+
+class FileParser():
+    """
+    Class FileParser tekes 3 arguments
+    file: path to file,
+    string_to_find: string
+    string_to_replace: string
+    """
+    def __init__(self, file, string_to_find, string_to_replace = None):
+        self.file = file
+        self.string_to_find = string_to_find
+        self.string_to_replace = string_to_replace
+    
+    def find_str(self):
+        """
+        Return count of string in file
+        """
+        count = 0
+        with open(self.file, encoding = "UTF-8") as f:
+            for line in f:
+                count += line.count(self.string_to_find)
+        return f"Find {count} strings"
+    
+    
+    def replace_str(self):
+        """
+        Replace one string another in file
+        and return the number of replacement
+        """
+        text = []
+        count = 0
+        with open(self.file, encoding = "UTF-8") as f:
+            for line in f.readlines():
+                count += line.count(self.string_to_find)
+                new_line = line.replace(self.string_to_find, self.string_to_replace)
+                text.append(new_line)
+        if text:
+            with open(self.file, 'w+', encoding = "UTF-8") as f:
+                for line in text:
+                    f.write(line)
+            return f"Replaced {count} strings"
+    
+    @staticmethod
+    def check_file_exists(path):
+        """
+        Check if path exists and if path is file
+        """
+        path = Path(r"" + path)
+        if not path.exists():
+            raise FileExistsError()
+        elif not path.is_file():
+            raise FileExistsError()
+        return path
+
+
+        
+
+
+if __name__ == "__main__":
+
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('file', type = FileParser.check_file_exists)
+        parser.add_argument('string_to_find')
+        parser.add_argument('-r', '--string_to_replace')
+        args =  parser.parse_args()
+        if args.string_to_replace:
+            logging.info(FileParser(**args.__dict__).replace_str())
+        else:
+            logging.info(FileParser(**args.__dict__).find_str())
+    except FileExistsError as e:
+        logging.error("Such file doesn't exists")
+        print("Such file doesn't exists")
+        
